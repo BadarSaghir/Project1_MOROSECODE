1ba511aac4ebcfe2902eb53498f11aec654646ea
SardarBadarSaghir <dataentrybadar@gmail.com>
2021-02-28 16:34:11 +0500

    added some addition functionalities

--- runserver.py
+++ runserver.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from flask.globals import request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=[ 'GET','POST'])
 def index():
+    if request.method == 'POST':
+        if request.form['Radios']=='encode':
+            return 'encoded'
+        elif request.form['Radios']=='decode':
+            return 'decoded'
+    return request.form['editor1']
     return render_template('index.html')
 
 if __name__ == '__main__':
